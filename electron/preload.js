5ec4aa900a03422739861af4e99c23cf8691d990
liu-xudong <z17609200438@163.com>
2021-12-01 14:56:21 +0800

    first commit

---
+++ electron/preload.js
@@ -0,0 +1,15 @@
+// 预加载
+
+// 所有Node.js API都可以在预加载过程中使用。
+// 它拥有与Chrome扩展一样的沙盒。
+window.addEventListener('DOMContentLoaded', () => {
+    const replaceText = (selector, text) => {
+        const element = document.getElementById(selector);
+        console.log(element);
+        if(element) element.innerText = text;
+    }
+
+    for(const dependency of ['chrome', 'node', 'electron']) {
+        replaceText(`${dependency}-version`, process.versions[dependency]);
+    }
+})
